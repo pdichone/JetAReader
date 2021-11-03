30bdc4909c2b0bc75df45c745d25c19a27811328
pdichone <pdichone@gmail.com>
2021-11-02 20:16:39 -0700

    Testing Firestore

--- app/src/main/java/com/bawp/freader/MainActivity.kt
+++ app/src/main/java/com/bawp/freader/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.bawp.freader
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
@@ -9,14 +10,44 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.bawp.freader.ui.theme.FReaderTheme
+import com.google.firebase.firestore.FirebaseFirestore
+import androidx.annotation.NonNull
+
+import com.google.android.gms.tasks.OnFailureListener
+
+import com.google.firebase.firestore.DocumentReference
+
+import com.google.android.gms.tasks.OnSuccessListener
+
+
+
+
+
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             FReaderTheme {
+
+                val db = FirebaseFirestore.getInstance()
+                val user: MutableMap<String, Any> = HashMap()
+                user["firsName"] = "Jeo"
+                user["lastName"] = "James"
+
+
+
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
+
+                    db.collection("users")
+                        .add(user)
+                        .addOnSuccessListener {
+                            Log.d("FB", "onCreate: ${it.id}")
+                        }.addOnFailureListener {
+                            Log.d("FB", "onCreate: $it")
+                        }
                     Greeting("Hello Today")
                 }
             }
